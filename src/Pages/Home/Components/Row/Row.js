40ca428f4dd550b5e8f34021487447a5692ba591
Cervantes Torres Ulises Emmanuel (RBEI/EBS3.2-MX) <ceu1ga@bosch.com>
2021-03-17 16:03:00 -0600

    STYLE - Row comp

--- src/Pages/Home/Components/Row/Row.js
+++ src/Pages/Home/Components/Row/Row.js
@@ -17,21 +17,14 @@ const Row = ({ genre}) => {
     const {name} = genre;
 
     const [movies,setMovies] = useState([]);
-    const [bgImg,setBgImg] = useState({});
 
-    const showMovieDetails = (el) => {
-        setSelectedMovie(el)
+    const showMovieDetails = (movieSelected) => {
+        setSelectedMovie(movieSelected)
         history.push('/movie')
     }
 
     const saveMovieLikeList = async () =>{
-        const res = await axios.get('/authentication/token/new?api_key=14d344666a5abe82c56c471106d9ecde')
-        const requestToken = res.data.request_token;
-
-
-        const data = await axios({ url: `/authenticate/${requestToken}?redirect_to=https://localhost:3000/`, baseURL: 'https://www.themoviedb.org' })
-        // await fetch(``)
-            
+        console.log('click')
     }
     
     useEffect( ()=> {
@@ -39,28 +32,24 @@ const Row = ({ genre}) => {
         const fetchMovies = async () =>{
             const request = await axios.get(requests.fetchMoviesByGenreID(genre.id))
             await setMovies( request.data.results )
-            const pathBGimg = request.data.results[0].backdrop_path
-            setBgImg({backgroundImage: `url( ${ requestImg.fetchMovieImg(pathBGimg) } )`})
 
-            return request;
+            return true;
         }
         fetchMovies();
     },[])
 
     return (
-        <div className='rowComponent' style={bgImg}>
-            <h1>{ name }</h1>
+        <div className='rowComponent' >
+            <h1 className='rowComponent__title'>{ name }</h1>
             <ul>
                 {
                     movies.length > 0 &&
                     movies.map( (el,i)=>{
                         return <li key={i}>
-                            <span>{el.name || el.title}</span><br/>
-                            <span className="like" onClick={saveMovieLikeList}>LIKE</span>
                             <LazyLoadImage
                             effect='blur'
-                                height="200px"
-                                width='180px'
+                                height="auto"
+                                width='90px'
                                 src={requestImg.fetchMovieImg(el.poster_path)} // use normal <img> attributes as props
                                 placeholderSrc={popCorn}
                                 onClick ={ ()=> showMovieDetails(el)}
